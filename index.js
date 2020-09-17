340c40b4c31fb72940d795bded0ec378453b5081
Edis Gonuler <edis@gonuler.com>
2020-09-17 09:56:10 -0400

    take notes on variables and booleans

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+let life = 100;
+
+life = life - 50;
+
+console.log(life)
+
+const purchased = True; // Boolean
+
+
+var name = "Edis";
+
+const pi = 3.14;
+
+console.log(pi);
+
+
+let start = null;
